d42bfbaa92650c30c21ee4b021b112a7d2ace413
Mungert69 <contact@mahadeva.co.uk>
2024-06-19 01:30:02 +0000

    updated download page for docker image version

--- src/components/main/Download.js
+++ src/components/main/Download.js
@@ -109,7 +109,7 @@ version: "3.8"
 
 services:
   networkmonitorprocessor:
-    image: mungert/networkmonitorprocessor:1.1.5
+    image: mungert/networkmonitorprocessor:1.1.6
     container_name: processor
     restart: always
     volumes:
